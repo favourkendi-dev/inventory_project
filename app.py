a6bfc2a198b06a4cb389c19d37743ef97d312e9f
favourkendi-dev <favour.kendi@student.moringaschool.com>
2026-07-16 17:26:02 +0300

    Update app.py for production to  bind to 0.0.0.0 and use PORT env variable

--- app.py
+++ app.py
@@ -393,4 +393,6 @@ def add_from_external():
 
 # Running  the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
